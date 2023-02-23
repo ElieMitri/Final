65bc7a6c48cd0177e39c03d94ed448ca2a03e68a
ElieMitri <eliegmitri7@gmail.com>
2023-02-23 21:00:01 +0200

    fixed home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import waiting from "./assets/waiting.png"
 
 const About = () => {
   return (
@@ -7,7 +8,9 @@ const About = () => {
     <div className="container">
       <div className="row">
         <div className="about__text--wrapper">
-          <h1 className="about__h1">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <div className='waiting__img--wrapper'>
+          <img className='waiting__img' src={waiting} alt="" />
+          </div>
           <h2 className="about__h2">FIND THE <span className="aqua--text">MOVIE</span> OF YOUR CHOICE.</h2>
           <Link to="/movies" className="about__a cursor">Browse Movies</Link>
         </div>
